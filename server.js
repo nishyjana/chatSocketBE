16a3a2e95ac73c7f70ff23e67b734e244f65a655
Nishanthan Janarthanarajah <nishanthanj@conceptvines.com>
2023-01-24 00:26:39 +0530

    init

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const messages = []
 
-app.get("/", (req, res) => {
-  res.send('hiii');
-});
 
 io.on('connection', (socket) => {
   const username = socket.handshake.query.username
@@ -24,5 +22,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3000, () => {
-  console.log("listening on *:3000");
+  console.log('listening on *:3000');
 });
